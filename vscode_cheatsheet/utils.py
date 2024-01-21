abe8c446d0aed83418c6db263bf556fb05738ecc
Josef Ondrej <josef.ondrej@outlook.com>
2024-01-21 17:35:17 +0100

    Add example class to utils.py

--- vscode_cheatsheet/utils.py
+++ vscode_cheatsheet/utils.py
@@ -1,2 +1,10 @@
 def my_util():
     print("my_util")
+
+
+class MyClass:
+    def __init__(self, name: str) -> None:
+        self._name = name
+
+    def introduce(self) -> None:
+        print(f"Hello, my name is {self._name}!")
